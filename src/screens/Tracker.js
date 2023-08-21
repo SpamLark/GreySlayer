4c5b10ffb246740be7e123c3f824ad89093244c5
SpamLark <51059253+SpamLark@users.noreply.github.com>
2023-08-21 13:02:44 +0100

    Refactored delete function chain

--- src/screens/Tracker.js
+++ src/screens/Tracker.js
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Animated, Alert} from 'react-native';
 import React, { useState, useEffect, useCallback } from 'react';
 import { getAllCheckIns } from '../services/database';
 import { useDatabase } from '../services/database/DatabaseContext';
@@ -11,29 +11,57 @@ const Tracker = () => {
   // Use shared database connection
   const db = useDatabase();
 
+  // Grab todays date using the helper function from services
+  const today = todaysDate();
+
   // Declare state variable checkIns to hold array of checkIn dates
   const [checkIns, setCheckIns] = useState([]);
 
-  // Declare empty object to receive formatted markedDates for the calendar
+  // Declare empty object to receive formatted markedDates for the calendar display
   const [markedDates, setMarkedDates] = useState({});
 
-  // Decalre current date for calendar focus using state to prevent calendar jumping when checkin is added / removed
-  const [focusDate, _] = useState(new Date());
+  // Declare state variable to track whether user has checked in today
+  const [checkedInToday, setCheckedInToday] = useState(false);
+
+  // Show alert dialogue confirming delete
+  const showDeleteAlert = (dateString) => {
+    Alert.alert(
+      'Delete Check-in',
+      'Are you sure you want to delete this check-in: ' + dateString,
+      [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'Delete', onPress: async () => {
+          // If user confirms deletion, attempt to delete the check-in
+          await handleDelete(dateString);
+        }}
+      ],
+      {cancelable: false}
+    )
+  }
+
+  // Handle the deletion of check-in identified from a date string
+  const handleDelete = async (dateString) => {
+    // Attempt the deletion
+    try {
+      await deleteCheckIn(db, dateString);
+      // Refresh check-ins with a fresh pull of check-in data
+      setCheckIns(await getAllCheckIns(db));
+      // If deleted value was today, set checkedInToday back to false to re-enable the check-in button
+      if (dateString === today) {
+        setCheckedInToday(false);
+      }
+    } catch (error) {
+      console.log('Error whilst deleting checkin:', error);
+    }
+  };
 
   // Define onDayPress behaviour
   const onDayPress = useCallback(async (day) => {
     // Check if pressed date has been checked in or not
     const dateHasCheckIn = checkIns.some(checkIn => checkIn.check_in_date === day.dateString);
-      try {
-          if (dateHasCheckIn) {
-            console.log(day.dateString, 'is in the array of checkIns');
-            await deleteCheckIn(db, day.dateString);
-          }
-          // Update the check-ins state after the delete operation
-          const currentCheckIns = await getAllCheckIns(db);
-          setCheckIns(currentCheckIns);
-      } catch (error) {
-        console.log(error)
+    // If the date has been checked in, start the delete sequence
+    if (dateHasCheckIn) {
+        showDeleteAlert(day.dateString);
       }
   }, [checkIns]);
 
@@ -42,7 +70,6 @@ const Tracker = () => {
     (async () => {
       try {
         setCheckIns(await getAllCheckIns(db));
-        //console.log(checkIns);
       } catch (error) {
         console.log('This is the error', error);
       }
@@ -62,23 +89,34 @@ const Tracker = () => {
     <View style={styles.container}>
       <View style={styles.calendarContainer}>
         {/*It's really important to set current for the CalendarList so that it renders quickly and avoids looking janky*/}
-          <CalendarList markedDates={markedDates} current={focusDate} onDayPress={onDayPress}/>
+          <CalendarList markedDates={markedDates} current={today} onDayPress={onDayPress}/>
       </View>
-      <TouchableOpacity 
+      <View style={styles.container}>
+        {!checkedInToday ? (
+          <TouchableOpacity 
             style={styles.button}
             onPress={
               async() => {
                 try {
-                await insertCheckIn(db, todaysDate());
-                setCheckIns(await getAllCheckIns(db));
+                  await insertCheckIn(db, today);
+                  setCheckIns(await getAllCheckIns(db));
+                  setCheckedInToday(true);
                 } catch (error) {
                   console.log(error);
                 }
               }
             }
-        >
-          <Text style={styles.buttonText}>Check In</Text>
-        </TouchableOpacity>
+          >
+            <Text style={styles.buttonText}>Check In</Text>
+          </TouchableOpacity>
+        ) : (
+          <>
+            <Text>Congratulations!</Text>
+            <Text>You checked in today</Text>
+          </>
+        )
+        }
+      </View>
     </View>
   );
 }
@@ -92,7 +130,7 @@ container: {
     marginTop: 0
 },
 calendarContainer: {
-  height: '60%'
+  height: '70%'
 },
 button: {
   paddingVertical: 8,
